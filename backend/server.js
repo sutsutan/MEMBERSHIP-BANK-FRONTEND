a1fd5d6ea3c419e71478b4f4e17ccb8966759c89
asabrightzzz <hannamariamonika123@gmail.com>
2025-11-10 13:33:23 +0700

    benerin api

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,10 @@ app.get('/api/anggota/:rfidTag', async (req, res) => {
         }
 
         // Response ke Frontend/Hardware
-        res.json(data[0]);
+        res.status(201).json({ 
+        message: 'Pendaftaran Berhasil.', 
+        member: data[0]
+    });
 
     } catch (error) {
         console.error('Error saat cek saldo:', error.message);
